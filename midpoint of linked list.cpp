ed5066afee6881625ce680688182218aa27e66e6
RIDDHI MONDAL <91659721+mondalriddhi@users.noreply.github.com>
2022-10-18 10:56:45 +0530

    Update midpoint of linked list.cpp

--- midpoint of linked list.cpp
+++ midpoint of linked list.cpp
@@ -1,3 +1,24 @@
+/*For a given singly linked list of integers, find and return the node present at the middle of the list.
+Note :
+If the length of the singly linked list is even, then return the first middle node.
+
+Example: Consider, 10 -> 20 -> 30 -> 40 is the given list, then the nodes present at the middle with respective data values are, 20 and 30.
+We return the first node with data 20.
+
+Sample Input 1 :
+1
+1 2 3 4 5 -1
+Sample Output 1 :
+3
+*************************
+Sample Input 2 :
+2 
+-1
+1 2 3 4 -1
+Sample Output 2 :
+2
+*/
+
 #include <iostream>
 
 class Node
