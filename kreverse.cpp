fd8a38c973df62437e1930277c9fd5e29dc1e74b
RIDDHI MONDAL <91659721+mondalriddhi@users.noreply.github.com>
2022-10-19 13:02:29 +0530

    Add files via upload

---
+++ kreverse.cpp
@@ -0,0 +1,109 @@
+/*  Given a singly linked list of integers, reverse the nodes of the linked list 'k' at a time and return its modified list.
+ 'k' is a positive integer and is less than or equal to the length of the linked list. If the number of nodes is not a multiple of 'k,' 
+ then left-out nodes,
+  in the end, should be reversed as well.
+Example :
+Given this linked list: 1 -> 2 -> 3 -> 4 -> 5
+
+For k = 2, you should return: 2 -> 1 -> 4 -> 3 -> 5
+
+For k = 3, you should return: 3 -> 2 -> 1 -> 5 -> 4
+
+*/
+
+
+#include <iostream>
+
+class Node
+{
+public:
+	int data;
+	Node *next;
+	Node(int data)
+	{
+		this->data = data;
+		this->next = NULL;
+	}
+};
+
+using namespace std;
+
+
+Node *takeinput()
+{
+	int data;
+	cin >> data;
+	Node *head = NULL, *tail = NULL;
+	while (data != -1)
+	{
+		Node *newnode = new Node(data);
+		if (head == NULL)
+		{
+			head = newnode;
+			tail = newnode;
+		}
+		else
+		{
+			tail->next = newnode;
+			tail = newnode;
+		}
+		cin >> data;
+	}
+	return head;
+}
+
+void print(Node *head)
+{
+	Node *temp = head;
+	while (temp != NULL)
+	{
+		cout << temp->data << " ";
+		temp = temp->next;
+	}
+	cout << endl;
+}
+
+Node *kReverse(Node *head, int k)
+{
+	//Write your code here
+    
+    if(head==NULL||k==1||k==0) {
+        return head;
+    }
+    Node *dummy_prev = NULL;
+    Node *current = head;
+    Node *nextNode = NULL;
+    
+    int count = 0;
+    while(count<k&&current!=NULL) {
+        nextNode = current -> next;
+        current -> next = dummy_prev;
+        dummy_prev = current;
+        current = nextNode;
+        count++;
+    }
+    
+    if(current!=NULL) {
+        head -> next = kReverse(current,k);
+    }
+    
+    return dummy_prev;
+    
+}
+
+int main()
+{
+	int t;
+	cin >> t;
+
+	while (t--)
+	{
+		Node *head = takeinput();
+		int k;
+		cin >> k;
+		head = kReverse(head, k);
+		print(head);
+	}
+	
+	return 0;
+}
