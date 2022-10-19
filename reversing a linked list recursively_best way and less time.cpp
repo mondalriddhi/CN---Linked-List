b01083e5af14eb843099a080244c5883823db0d9
RIDDHI MONDAL <91659721+mondalriddhi@users.noreply.github.com>
2022-10-19 12:03:22 +0530

    Add files via upload

---
+++ reversing a linked list recursively_best way and less time.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+
+class Node
+{
+public:
+	int data;
+	Node *next;
+	Node(int data)
+	{
+		this->data = data;
+		this->next = NULL;
+	}
+};
+
+using namespace std;
+
+
+Node *takeinput()
+{
+	int data;
+	cin >> data;
+	Node *head = NULL, *tail = NULL;
+	while (data != -1)
+	{
+		Node *newnode = new Node(data);
+		if (head == NULL)
+		{
+			head = newnode;
+			tail = newnode;
+		}
+		else
+		{
+			tail->next = newnode;
+			tail = newnode;
+		}
+		cin >> data;
+	}
+	return head;
+}
+
+void print(Node *head)
+{
+	Node *temp = head;
+	while (temp != NULL)
+	{
+		cout << temp->data << " ";
+		temp = temp->next;
+	}
+	cout << endl;
+}
+
+Node *reverseLinkedListRec(Node *head)
+{
+    //Write your code here
+    if(head==NULL||head->next==NULL) {
+        return head;
+    }
+   
+    Node *ans = reverseLinkedListRec(head -> next);
+    
+    Node *tail = head -> next;
+    tail -> next = head;
+    head -> next = NULL;
+  
+    return ans;
+}
+
+int main()
+{
+	int t;
+	cin >> t;
+
+	while(t--)
+	{
+		Node *head = takeinput();
+		head = reverseLinkedListRec(head);
+		print(head);
+	}
+
+	return 0;
+}
